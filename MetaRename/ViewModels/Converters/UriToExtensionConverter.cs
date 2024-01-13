4678f4f69a81bf04933a60131ee576589f179886
LeonSteinbach <leon@steinba.de>
2024-01-13 23:54:32 +0100

    Added converter

---
+++ MetaRename/ViewModels/Converters/UriToExtensionConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace MetaRename.ViewModels.Converters;
+
+public class UriToExtensionConverter : IValueConverter
+{
+    public static readonly UriToExtensionConverter Instance = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is string sourceText && targetType.IsAssignableTo(typeof(string))) {
+            return Path.GetExtension(sourceText);
+        }
+
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return value;
+    }
+}
